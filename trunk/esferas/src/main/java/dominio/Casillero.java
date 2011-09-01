cc52c095f752b1a1f0421f2f5f6c3754c05e45e5
cdmarchionne@gmail.com <cdmarchionne@gmail.com@7ca026f7-984a-3735-c18d-15b97964c2d9>
2011-09-01 14:53:30 +0000

    Creacion del Proyecto

---
+++ trunk/esferas/src/main/java/dominio/Casillero.java
@@ -0,0 +1,17 @@
+package dominio;
+
+public class Casillero {
+	
+	private ObjetosDragonBall objeto;
+	
+	public void setObjeto(ObjetosDragonBall objeto) {
+		this.objeto = objeto;
+	}
+
+	public ObjetosDragonBall getObjeto() {
+		return objeto;
+	}
+	
+	
+
+}
---
+++ trunk/esferas/src/main/java/dominio/ObjetosDragonBall.java
@@ -0,0 +1,7 @@
+package dominio;
+
+public interface ObjetosDragonBall {
+
+	String getNombre();
+
+}
