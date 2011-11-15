9222c2ce30a7562366f1007d33225cc1ceaca97d
cdmarchionne@gmail.com <cdmarchionne@gmail.com@7ca026f7-984a-3735-c18d-15b97964c2d9>
2011-11-15 23:49:59 +0000

    Se logró manejar de manera exitosa un lista tipo combo bindeada directamente con el modelo

--- trunk/esferas-wicket/src/main/java/ar/edu/unq/tpi/esferas_wicket/MapaPage.java
+++ trunk/esferas-wicket/src/main/java/ar/edu/unq/tpi/esferas_wicket/MapaPage.java
@@ -2,23 +2,54 @@ package ar.edu.unq.tpi.esferas_wicket;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.RequiredTextField;
-import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.uqbar.commons.model.UserException;
 
 import utils.Punto;
+import dominio.Esfera;
 import dominio.Mapa;
+import dominio.Personaje;
 
 public class MapaPage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
     public MapaPage(final Mapa mapa) {
-    	this.add(new Label("dimX", mapa.getDimension().getX().toString()));
-    	this.add(new Label("dimY", mapa.getDimension().getY().toString()));
-	}
+    	this.add(new Label("dim", new PropertyModel<Punto<Integer>>(mapa, Mapa.DIMENSION)));
+    	this.add(new DropDownChoice<Esfera>("esferas", mapa.getListaEsferas(), new IChoiceRenderer<Esfera>() {
+	         // Gets the display value that is visible to the end user.
+	        public String getDisplayValue(Esfera esfera) {
+				return esfera.getName();
+	        }
+
+	         // Gets the value that is invisble to the end user, and that is used as the selection id.
+	        public String getIdValue(Esfera esfera, int index) {
+				return ((Integer) index).toString();
+	        }
+	    }));
+    	
+    	this.add(new DropDownChoice<Personaje>("personajes", mapa.getListaPersonajes(), new IChoiceRenderer<Personaje>() {
+	         // Gets the display value that is visible to the end user.
+	        public String getDisplayValue(Personaje personaje) {
+				return personaje.getName();
+	        }
+
+	         // Gets the value that is invisble to the end user, and that is used as the selection id.
+	        public String getIdValue(Personaje personaje, int index) {
+				return ((Integer) index).toString();
+	        }
+	    }));
+    	
+
+    	this.add(new DropDownChoice<Esfera.CantidadEstrellas>("sinEsferas", mapa.getListaEsferasNoCreadas()));
+    	this.add(new DropDownChoice<Personaje.NombrePersonaje>("sinPersonajes", mapa.getListaPersonajesNoCreadas()));
+    	
+    }
+
+//	private void add(DropDownChoice<Esfera> dropDownChoice,
+//			IChoiceRenderer<Esfera> iChoiceRenderer) {
+//		// TODO Auto-generated method stub
+//		
+//	}
     
 }
