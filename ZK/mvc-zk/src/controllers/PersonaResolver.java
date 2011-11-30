dfee6fe69f374eb618c6672a5505706489b32704
cdmarchionne@gmail.com <cdmarchionne@gmail.com@7ca026f7-984a-3735-c18d-15b97964c2d9>
2011-11-30 23:38:02 +0000

    Correccion de los ejemplos

--- ZK/mvc-zk/src/controllers/PersonaResolver.java
+++ ZK/mvc-zk/src/controllers/PersonaResolver.java
@@ -1,16 +1,12 @@
 package controllers;
 
-import org.zkoss.xel.XelException;
-
 import domain.formulario.ListaPersonas;
-import domain.formulario.Persona;
 
-public  class PersonaResolver implements org.zkoss.xel.VariableResolver{
+public class PersonaResolver implements org.zkoss.xel.VariableResolver {
 
-	
 	public Object resolveVariable(String name) {
-		
-		ListaPersonas lp = new ListaPersonas();
+
+		ListaPersonas lp = ListaPersonas.getInstance();
 		return "personas".equals(name) ? ListaPersonas.getListaPersonas() : null;
 	}
 
