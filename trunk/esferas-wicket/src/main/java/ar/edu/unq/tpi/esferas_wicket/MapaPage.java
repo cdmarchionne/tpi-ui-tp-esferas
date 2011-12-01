d072918fd2e5a6d07c6fa44c0389f9f13500ab13
cdmarchionne@gmail.com <cdmarchionne@gmail.com@7ca026f7-984a-3735-c18d-15b97964c2d9>
2011-12-01 06:28:32 +0000

    Se muestra una tabla con los detalles de los Personajes en la página del Mapa. Cambio de Stylos en el CSS

--- trunk/esferas-wicket/src/main/java/ar/edu/unq/tpi/esferas_wicket/MapaPage.java
+++ trunk/esferas-wicket/src/main/java/ar/edu/unq/tpi/esferas_wicket/MapaPage.java
@@ -4,7 +4,11 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 
 import utils.Punto;
@@ -37,6 +41,29 @@ public class MapaPage extends PosicionablePage {
 		this.add(formBusqueda);
 		this.addFeedbackPanel(formBusqueda);
 		this.addButtonsBusqueda(formBusqueda);
+		
+		this.add(detallePersonaje());
+	}
+
+	/**
+	 * Genera una Tabla con los detalles de los personajes
+	 * @return
+	 */
+	private ListView<Personaje> detallePersonaje() {
+		return new ListView<Personaje>("personajes", this.getMapa().getListaPersonajes()) {
+			private static final long serialVersionUID = 1L;
+			// This method is called for each 'entry' in the list.
+			@Override 
+			protected void populateItem(ListItem<Personaje> item) {
+				Personaje personaje = (Personaje) item.getModelObject();
+				item.add(new Image("imagen", new Model("images/"+personaje.getName().toLowerCase()+".png")));
+				item.add(new Label(Personaje.NOMBRE, personaje.getName()));
+				item.add(new Label("posicion", personaje.getPosicion().toString()));
+				item.add(new Label(Personaje.DISTANCIA, personaje.getDistancia().toString()));
+				item.add(new Label("cantidad", String.valueOf(personaje.getInventario().size())));
+				item.add(new Label(Personaje.INVENTARIO, personaje.getInventario().toString()));
+			}		
+		};
 	}
 	
 	public MapaPage(PosicionablePage paginaOrigen) {
